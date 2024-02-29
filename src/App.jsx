a9557e22c91cd68962f133f4596a0247594b6a5d
Faizan <faizanalam980@gmail.com>
2024-02-29 20:51:41 +0530

    CSS for table, searchbar and dropdown

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,86 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React, { useState, useEffect } from 'react';
+import { onValue, ref } from 'firebase/database';
+import {db} from './firebase';
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [employees, setEmployees] = useState([]);
+  //search state
+  const [searchTerm, setSearchTerm] = useState('');
+//dropwdown for number of rows with 5 default
+  const [rowsToShow, setRowsToShow] = useState(5);
+
+
+  //to fetch data from employees node from database
+  useEffect(() => {
+    const dbRef = ref(db, 'employees');
+    const handleData = snap => {
+      if (snap.val()) setEmployees(Object.values(snap.val()));
+    }
+
+    onValue(dbRef, handleData, {
+      onlyOnce: true,
+    });
+
+    //cleanup funcition
+    return () => {
+      onValue(dbRef, null);
+    };
+
+  }, []);
+
+  // Filter employees based on search term
+  const filteredEmployees = employees.filter(employee => {
+    const fullName = `${employee.name} ${employee.position} ${employee.department}`.toLowerCase();
+    return fullName.includes(searchTerm.toLowerCase());
+  });
+
+  // Slice the filtered employees array based on the number of rows to show
+  const slicedEmployees = filteredEmployees.slice(0, rowsToShow);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <h1>React Assessment</h1>
+      <h3>Employee Data</h3>
+      <div className="filter-container">
+        <input
+          type="text"
+          placeholder="Search..."
+          value={searchTerm}
+          onChange={e => setSearchTerm(e.target.value)}
+          className="search-input"
+        />
+        <select
+          value={rowsToShow}
+          onChange={e => setRowsToShow(parseInt(e.target.value))}
+          className="rows-dropdown"
+        >
+          <option value={5}>5 Rows</option>
+          <option value={10}>10 Rows</option>
+          <option value={15}>15 Rows</option>
+        </select>
+
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Age</th>
+            <th>Position</th>
+            <th>Department</th>
+          </tr>
+        </thead>
+        <tbody>
+        {slicedEmployees.map((employee, index) => (
+            <tr key={index}>
+              <td>{employee.name}</td>
+              <td>{employee.age}</td>
+              <td>{employee.position}</td>
+              <td>{employee.department}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </>
   )
 }
